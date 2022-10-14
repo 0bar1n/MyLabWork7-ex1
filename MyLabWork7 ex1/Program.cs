52cbb010e6920dc247d8414078a3b394885205ac
barinskaya <1388zxc@gmail.com>
2022-10-14 14:43:14 +0300

    Добавьте файлы проекта.

---
+++ MyLabWork7 ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyLabWork7_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a; //переменная десятичного типа
+            Console.WriteLine("Двузначное число a="); //просим пользователя ввести число
+            a = Convert.ToDecimal(Console.ReadLine()); //задание переменной десятичного типа
+            a = Math.Floor(a / 10); // задание переменной по формуле, нахождение числа дeсятков в ней 
+            Console.WriteLine($"Число десятков в числе а равно {a}"); //вывод на экран 
+        }
+    }
+}
